2cd50bcbf9329558aaa530233acacb8db9664bbf
Favilances <78090594+favilances@users.noreply.github.com>
2026-06-02 17:14:53 +0300

    refactor: add iyzico.go stub, remove old stripe.go placeholder

---
+++ server/routes/iyzico.go
@@ -0,0 +1,5 @@
+package routes
+
+// Iyzico entegrasyonu için hazırlık.
+// Gerçek ödeme sağlayıcısı eklendiğinde bu dosyaya taşınacak.
+// Mevcut durumda bağışlar sanal cüzdan üzerinden işlenmektedir.
--- server/routes/stripe.go
+++
@@ -1,3 +0,0 @@
-package routes
-
-// Legacy Stripe dosyası boş bırakıldı. Yeni ödeme rotaları `iyzico.go` içinde yer alıyor.
