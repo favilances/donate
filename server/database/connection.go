60e4e24f3c3572cbc2df04bdbf59baa1f81ee50a
Favilances <78090594+favilances@users.noreply.github.com>
2026-06-02 17:11:02 +0300

    feat: add unique indexes on email and username to prevent duplicates

--- server/database/connection.go
+++ server/database/connection.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -26,7 +27,28 @@ func Connect(ctx context.Context, uri string, dbName string) error {
 
 	client = mongoClient
 	db = client.Database(dbName)
+
+	if err := ensureIndexes(ctx); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func ensureIndexes(ctx context.Context) error {
+	users := db.Collection("users")
+
+	_, err := users.Indexes().CreateMany(ctx, []mongo.IndexModel{
+		{
+			Keys:    bson.D{{Key: "email", Value: 1}},
+			Options: options.Index().SetUnique(true),
+		},
+		{
+			Keys:    bson.D{{Key: "username", Value: 1}},
+			Options: options.Index().SetUnique(true),
+		},
+	})
+	return err
 }
 
 func Database() *mongo.Database {
